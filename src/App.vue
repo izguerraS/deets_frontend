7547aee0ba7f8a8cb5226d422de4b15d8afefd2e
Sam <Izguerra.sam@gmail.com>
2020-10-21 13:33:09 -0500

    added signup/login/logout buttons

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+      <a class="navbar-brand" href="index.html">The Cookbook</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
@@ -11,16 +11,17 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="index.html">Home</a>
+            <button class="navbar-brand" href="/signup" v-if="!isLoggedIn()" ><router-link to="/signup">Signup</router-link></button>
+          </li>
+          <hr>
+          <li class="nav-item">
+            <button class="navbar-brand" href="/login" v-if="!isLoggedIn()" ><router-link to="/login">login</router-link></button>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/recipes/new">Make a New Recipe</a>
+            <button class="navbar-brand" href="/logout" v-if="isLoggedIn()" ><router-link v-if="isLoggedIn()" to="/logout">Logout</router-link></button>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="post.html">Sample Post</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact</a>
+            <button class="navbar-brand" href="/recipes/new" v-if="isLoggedIn()" ><router-link v-if="isLoggedIn()" to="/recipes/new">New Recipe</router-link></button>
           </li>
         </ul>
       </div>
@@ -28,14 +29,15 @@
   </nav>
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+  <header class="masthead" style="background-image: url('/img/Mexican_food.jpg')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
-            <h1>Clean Blog</h1>
-            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+            <h1>The Cookbook</h1>
+            <span class="subheading">
+               Digital Cookbook to Share Our Family's Favorite Recipes. </span>
           </div>
         </div>
       </div>
